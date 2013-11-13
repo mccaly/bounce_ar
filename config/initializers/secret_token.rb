8b4667600fee73c7bf217116ea1043e2ddba9c1d
Andrew McCalister <andrewmccalister@googlemail.com>
2013-11-12 16:14:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BounceAr::Application.config.secret_key_base = '68f7161bb45986d0a69b538e3e95e3b50bef12a0f7c096a5c1b4a1731c6fe64fff77523659de9ef4abee9b5627ecfe925963c4ec4ba0b439b7d828f4477fef6b'
